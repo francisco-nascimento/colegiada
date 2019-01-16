bce52664fad8bf6a5577b431870a78972ad49597
fnascimento <chico.nascimento@gmail.com>
2019-01-16 01:31:13 -0300

    Adição do Spring Security: - CRUD de Usuario - Controle de Permissoes

--- ProvaColegiada/src/main/java/br/ifpe/pg/provacolegiada/Home.java
+++ ProvaColegiada/src/main/java/br/ifpe/pg/provacolegiada/Home.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
-
 	
-	@GetMapping("/")
+	@GetMapping({"/","/home"})
 	public String exibirHome() {
 		return "home";
 	}
--- ProvaColegiada/src/main/java/br/ifpe/pg/provacolegiada/cadastro/CursoController.java
+++ ProvaColegiada/src/main/java/br/ifpe/pg/provacolegiada/cadastro/CursoController.java
@@ -3,6 +3,7 @@ package br.ifpe.pg.provacolegiada.cadastro;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/cursos/")
+@Secured("ROLE_ADMIN")
 public class CursoController {
 
 	@Autowired
